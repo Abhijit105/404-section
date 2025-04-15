f43e6014b6a7ff28711bdbc744a85e89dddb6c3e
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2025-04-15 10:52:48 +0530

    config: vite to include the image in build

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: './',
+  build: {
+    assetsDir: 'img', // This will put assets in dist/img instead of dist/assets
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          // Don't hash image files
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `img/[name].[ext]`
+          }
+          return `assets/[name]-[hash].[ext]`
+        },
+      },
+    },
+  },
 })
